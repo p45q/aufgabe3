f3173250d31ab558594f3c9cedd30cad482528cf
Thierry Baumann <thierry@swissmademarketing.com>
2013-05-24 02:06:08 +0200

    mail added

---
+++ QuickMailer/src/mail/Mail.java
@@ -0,0 +1,48 @@
+package mail;
+
+public class Mail {
+	String from;
+	String to;
+	String subject;
+	String body;
+	MailAccount mailaccount;
+	
+	public Mail(String from, String to, String subject, String body, MailAccount mailaccount) {
+		this.from = from;
+		this.to = to;
+		this.subject = subject;
+		this.body = body;
+		this.mailaccount = mailaccount;
+	}
+	public String getFrom() {
+		return from;
+	}
+	public void setFrom(String from) {
+		this.from = from;
+	}
+	public String getTo() {
+		return to;
+	}
+	public void setTo(String to) {
+		this.to = to;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+	public MailAccount getMailaccount() {
+		return mailaccount;
+	}
+	public void setMailaccount(MailAccount mailaccount) {
+		this.mailaccount = mailaccount;
+	}
+
+}
