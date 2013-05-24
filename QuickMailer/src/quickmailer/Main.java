c04904f0d96f561b2292e7d11d549c1748b989e9
Thierry Baumann <thierry@swissmademarketing.com>
2013-05-24 02:05:39 +0200

    getSystemLookAndFeelClassName

--- QuickMailer/src/quickmailer/Main.java
+++ QuickMailer/src/quickmailer/Main.java
@@ -4,15 +4,20 @@ import gui.QuickmailGui;
 
 import javax.swing.JFrame;
 import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class Main {
 
 	/**
 	 * @param args
+	 * @throws UnsupportedLookAndFeelException 
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
+	 * @throws ClassNotFoundException 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		
 		JFrame f = new QuickmailGui();
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // wichtig!
 		f.setSize(600, 400); // oder: f.pack();
